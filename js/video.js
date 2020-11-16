117d898b6915dd9ffe6666dfa9032ff1589f3509
jacktucker8 <70600861+jacktucker8@users.noreply.github.com>
2020-11-16 18:30:25 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -5,12 +5,12 @@ window.addEventListener("load", function() {
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+	video.play();
 
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	console.log ("Pause Video");
+	video.pause();
 	
 });
 
