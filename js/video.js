4a399284fe63cb0859b2a73249a0c76f858d0f60
jacktucker8 <70600861+jacktucker8@users.noreply.github.com>
2020-11-17 20:24:20 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -33,7 +33,7 @@ document.querySelector("#skip").addEventListener("click", function() {
 document.querySelector("#mute").addEventListener("click", function() {
 	if (video.muted== false){
 		video.muted= true
-		document.getElementById("mute").innerText="unmute"}
+		document.getElementById("mute").innerText="Unmute"}
 		else{
 		video.muted= false
 		document.getElementById("mute").innerText="Mute"}
