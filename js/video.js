5f40a17b5d58dc08365a192fce34980573142f91
jacktucker8 <70600861+jacktucker8@users.noreply.github.com>
2020-11-18 22:44:14 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -41,7 +41,7 @@ document.querySelector("#mute").addEventListener("click", function() {
 });
 
 document.querySelector("#old").addEventListener("click", function() {
-	document.getElementById("myVideo").classList.replace("oldTime")
+	document.getElementById("myVideo").classList.toggle("oldTime")
 });
 
 
